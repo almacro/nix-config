0e439d1f82841636f59f7f1349c380e5bfd46d6e
Alfred Thompson <almacro@Blackbook.local>
2026-02-15 13:51:17 -0800

    initial working nix-darwin + home-manager setup

---
+++ modules/darwin/common/default.nix
@@ -0,0 +1,72 @@
+{
+  pkgs,
+  userConfig,
+  ...
+}:
+{
+  # Determinate Nix manages nix itself; disable nix-darwin's nix management
+  nix.enable = false;
+
+  # User configuration
+  users.users.${userConfig.name} = {
+    name = userConfig.name;
+    home = "/Users/${userConfig.name}";
+  };
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    mg
+    tmux
+    vim
+  ];
+
+  # Networking / firewall
+  networking.applicationFirewall.enable = true;
+  networking.applicationFirewall.enableStealthMode = true;
+  networking.applicationFirewall.allowSigned = true;
+  networking.applicationFirewall.allowSignedApp = true;
+  networking.computerName = "Blackbook";
+
+  # Zsh
+  programs.zsh.enable = true;
+
+  # System settings
+  system = {
+    primaryUser = userConfig.name;
+    defaults = {
+      CustomUserPreferences = {
+        # Disable siri
+        "com.apple.Siri" = {
+          "UAProfileCheckingStatus" = 0;
+          "siriEnabled" = 0;
+        };
+        # Disable personalized ads
+        "com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false;
+        };
+      };
+    };
+  };
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    source-code-pro
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
+  ];
+
+  # Homebrew (managed via nix-darwin)
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "zap";
+    onActivation.autoUpdate = false;
+    onActivation.upgrade = true;
+
+    casks = [
+      "emacs-app"
+      "firefox"
+      "vscodium"
+      #"vlc"
+    ];
+  };
+}
---
+++ nix-black/modules/home-manager/common/default.nix
@@ -0,0 +1,45 @@
+{
+  userConfig,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  # Home-Manager configuration
+  home = {
+    username = userConfig.name;
+    homeDirectory =
+      if pkgs.stdenv.hostPlatform.isDarwin then
+        "/Users/${userConfig.name}"
+      else
+        "/home/${userConfig.name}";
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = lib.mkIf (!pkgs.stdenv.hostPlatform.isDarwin) "sd-switch";
+
+  # Common packages
+  home.packages = with pkgs; [
+    git
+  ];
+
+  # Zsh configuration
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "starship";
+    };
+
+    initContent = ''
+      if [ -f "$HOME/.zprofile" ]; then
+        source "$HOME/.zprofile"
+      fi
+    '';
+  };
+}
